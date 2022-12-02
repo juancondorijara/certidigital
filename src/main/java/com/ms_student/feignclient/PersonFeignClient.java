aab9c5b19293439e9eabc97a29b4492dc7ffae90
Juan Gabriel Condori Jara <juan.condori.jara@vallegrande.edu.pe>
2022-12-02 16:50:04 -0500

    PersonFeignClient.java

--- src/main/java/com/ms_student/feignclient/PersonFeignClient.java
+++ src/main/java/com/ms_student/feignclient/PersonFeignClient.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import reactivefeign.spring.config.ReactiveFeignClient;
 import reactor.core.publisher.*;
 
+//@ReactiveFeignClient(value = "ms-person", url = "http://localhost:8085/v2/person")
 @ReactiveFeignClient(value = "ms-person", url = "https://certidigital-ms-person-be.azurewebsites.net/v2/person")
 public interface PersonFeignClient {
 
